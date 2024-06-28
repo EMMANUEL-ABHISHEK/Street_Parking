ec070666a617eece2d67b67a84f37a8b1ca80398
EMMANUEL-ABHISHEK <140959479+EMMANUEL-ABHISHEK@users.noreply.github.com>
2024-06-28 15:55:49 +0530

    app.py

--- app.py
+++ app.py
@@ -5,6 +5,9 @@ from twilio.rest import Client
 from config import Config
 from models.resnet50_model import analyze_image
 import cv2
+from dotenv import load_dotenv
+
+load_dotenv()
 
 app = Flask(__name__)
 app.config.from_object(Config)
@@ -16,12 +19,6 @@ twilio_client = Client(app.config['TWILIO_ACCOUNT_SID'], app.config['TWILIO_AUTH
 def allowed_file(filename):
     return '.' in filename and filename.rsplit('.', 1)[1].lower() in app.config['ALLOWED_EXTENSIONS']
 
-def calculate_dynamic_price(current_demand, base_price):
-    max_price = base_price * 2
-    min_price = base_price * 0.5
-    price = base_price * (1 + current_demand / 100)
-    return max(min(price, max_price), min_price)
-
 @app.route('/')
 def index():
     return render_template('index.html')
@@ -30,22 +27,22 @@ def index():
 def upload_file():
     if 'file' not in request.files:
         return "No file part"
-    
+
     file = request.files['file']
-    
+
     if file.filename == '':
         return "No selected file"
-    
+
     if file and allowed_file(file.filename):
         file_path = os.path.join(app.config['UPLOAD_FOLDER'], file.filename)
         file.save(file_path)
 
-        analysis_result = analyze_image(img_path=file_path)
+        analysis_result = analyze_image(file_path)
 
         if analysis_result is not None:
             now = datetime.now()
             current_date = now.strftime("%Y-%m-%d")
-            output_csv_path = os.path.join(os.path.dirname(os.path.realpath(_file_)), f"{current_date}_parking_results.csv")
+            output_csv_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), f"{current_date}_parking_results.csv")
 
             with open(output_csv_path, 'a', newline='') as f:
                 f.write(f"{file.filename},{analysis_result}\n")
@@ -53,7 +50,7 @@ def upload_file():
             twilio_client.messages.create(
                 body=f"Your parking spot {file.filename} is detected. Analysis result: {analysis_result}",
                 from_=app.config['TWILIO_PHONE_NUMBER'],
-                to='user_phone_number'
+                to='user_phone_number'  # Replace with recipient phone number
             )
 
             return redirect(url_for('index'))
@@ -69,7 +66,7 @@ def generate_frames():
             break
         else:
             # Analyze the frame
-            analysis_result = analyze_image(img_array=frame)
+            analysis_result = analyze_image(frame)
 
             # Draw the analysis result on the frame
             if analysis_result is not None:
