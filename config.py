6f2c77ba4d3cab752ffc17577dbe5663cf6cd5b5
Emmanuel Abhishek Karuturi <140959479+EMMANUEL-ABHISHEK@users.noreply.github.com>
2024-07-07 23:02:30 -0700

    config.py

--- config.py
+++ config.py
@@ -2,7 +2,7 @@ import os
 
 class Config:
     UPLOAD_FOLDER = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'uploads')
-    ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg','gif'}
+    ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg','gif','.mp4'}
     TWILIO_ACCOUNT_SID = 'AC723ff75eb26c998305921ba3386d4806'
     TWILIO_AUTH_TOKEN = '4ec7108c543ae4a211f584379b2e9bd8'
     TWILIO_PHONE_NUMBER = 'your_twilio_phone_number'
